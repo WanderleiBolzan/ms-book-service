3ae62dad0c55a283eaa4ec2bb4befb1fbd6acaf8
wanderlei jose bolzan <wbolzanjp@gmail.com>
2026-01-23 16:33:47 -0300

    Início da criação da entidade Book e do seu respectivo Controller

---
+++ src/main/java/br/com/wanderlei/controller/BookController.java
@@ -0,0 +1,28 @@
+package br.com.wanderlei.controller;
+
+import br.com.wanderlei.environment.InstanceInformationService;
+import br.com.wanderlei.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("book-service")
+public class BookController {
+
+    @Autowired
+    private InstanceInformationService informationService;
+
+    //http://localhost:8100/book-service/1/BRL
+    @GetMapping(value="/{id}/{currency}", produces = MediaType.APPLICATION_JSON_VALUE )
+    public Book findBook(
+            @PathVariable("id") Long id,
+            @PathVariable("currency") String currency
+    ) {
+        return new Book();
+    }
+
+}
