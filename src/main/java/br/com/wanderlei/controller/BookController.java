995bebc8a749454c579e06ec3029d95822353d63
wanderlei jose bolzan <wbolzanjp@gmail.com>
2026-01-24 22:26:37 -0300

    integrando e consumindo o microserviço exchange-service no ms book-service

--- src/main/java/br/com/wanderlei/controller/BookController.java
+++ src/main/java/br/com/wanderlei/controller/BookController.java
@@ -1,6 +1,7 @@
 package br.com.wanderlei.controller;
 
 import br.com.wanderlei.BookRepository;
+import br.com.wanderlei.dto.Exchange;
 import br.com.wanderlei.environment.InstanceInformationService;
 import br.com.wanderlei.model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("book-service")
@@ -27,6 +31,34 @@ public class BookController {
             @PathVariable("currency") String currency
     ) {
         String port = informationService.retrieverServerPort ();
+
+        var book = repository.findById(id).orElseThrow();
+
+        HashMap<String, String> parans = new HashMap<>();
+        parans.put ("amount", book.getPrice ().toString ());
+        parans.put ("from", "USD");
+        parans.put ("to", currency);
+
+        var response = new RestTemplate()
+                .getForEntity ("http://localhost:8000/exchange-service/{amount}/{from}/{to}", Exchange.class, parans);
+
+        Exchange exchange = response.getBody ();
+
+        book.setEnvironment (port);
+        book.setPrice (exchange.getConvertedValue ());
+        book.setCurrency (currency);
+
+        return book;
+    }
+
+    /*
+    //http://localhost:8100/book-service/1/BRL
+    @GetMapping(value="/{id}/{currency}", produces = MediaType.APPLICATION_JSON_VALUE )
+    public Book findBook(
+            @PathVariable("id") Long id,
+            @PathVariable("currency") String currency
+    ) {
+        String port = informationService.retrieverServerPort ();
         var book = repository.findById(id).orElseThrow();
 
         book.setEnvironment (port);
@@ -34,5 +66,5 @@ public class BookController {
 
         return book;
     }
-
+*/
 }
