c555098a60f3878cfbe64fd3b98da54982e8990c
wanderlei jose bolzan <wbolzanjp@gmail.com>
2026-01-28 10:27:48 -0300

    Início da implementação do Circuit Break

---
+++ src/main/java/br/com/wanderlei/controller/FooBarController.java
@@ -0,0 +1,25 @@
+package br.com.wanderlei.controller;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping ("book-service")
+@Retry (name="default")
+public class FooBarController {
+
+    private Logger logger = LoggerFactory.getLogger (FooBarController.class);
+    @GetMapping("/foo-bar")
+    public String fooBar(){
+        logger.info ("Request to foo-bar is Received");
+        var response = new RestTemplate()
+                .getForEntity ("http://localhost:8082/foo-bar", String.class);
+        return response.getBody ();
+    }
+
+}
